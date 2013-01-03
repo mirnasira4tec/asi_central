7e4c2a2d49108baa6add0f69fae803b942b75d80
Ngon Hua <nhua@asicentral.com>
2013-01-02 21:53:24 -0500

    ngon's commit
    Ngon's first commit

---
+++ Projects/Template/src/Web Application/Controllers/NgonController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asi.asicentral.web.Controllers
+{
+    public class NgonController : Controller
+    {
+        public ActionResult Ngon()
+        {
+            ViewBag.Title = "Ngon";
+            ViewBag.Message = "First commit";
+            return View();
+        }
+
+    }
+}
