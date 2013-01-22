b46262f977420c71b182a7a1685c72609e2b49cf
noh5017 <nhua@asicentral.com>
2013-01-21 23:32:35 -0500

    Products

---
+++ Projects/ASI Admin/src/Web Application/Controllers/sgr/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using asi.asicentral.services.interfaces;
+
+namespace asi.asicentral.web.Controllers.sgr
+{
+    public class ProductController : Controller
+    {
+        private IObjectService _objectService;
+
+        public ProductController(IObjectService objectService)
+        {
+            _objectService = objectService;
+        }
+
+        public ActionResult List()
+        {
+            return View("../sgr/Product/Index");
+        }
+
+    }
+}
