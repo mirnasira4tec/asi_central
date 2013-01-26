a9f4da17f783e5b47b3ed92a9749e272c76ea5b6
Yann Perrin <perrin.yann@googlemail.com>
2013-01-26 18:53:10 -0500

    Added a form push supporting Authentication Token into asi.util.submit() Removing the Div when calling Dialog Updated applications to use new functionality

---
+++ Projects/ASI Admin/src/Web Application/Controllers/TemplateController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asi.asicentral.web.Controllers
+{
+    public class TemplateController : Controller
+    {
+        public ActionResult Form()
+        {
+            return View("Form");
+        }
+        public ActionResult Dialog()
+        {
+            return View("Dialog");
+        }
+    }
+}
