6fa231f715471f558acb43bbd8178728af7b56cb
noh5017 <nhua@asicentral.com>
2013-02-14 16:59:05 -0500

    Changed property of Price and Priceceiling to Decimal for object entity Products.
    Minor change to sgr tool desktop application.
    Created view models for Orders and order detail.
    Create application controller to receive supplier and distributor application.

---
+++ Projects/ASI Admin/src/Web Application/Models/Store/OpenOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asi.asicentral.web.Models.Store
+{
+    public class OpenOrder
+    {
+        public int OrderId { get; set; }
+        public String Name { get; set; }
+        public String Company { get; set; }
+        public Nullable<int> Phone { get; set; }
+        public String Product { get; set; }
+        public String BillingAddress { get; set; }
+        public DateTime DateOrerCreated { get; set; }
+    }
+}
---
+++ Projects/ASI Admin/src/Web Application/Models/Store/ViewOrders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asi.asicentral.web.Models.Store
+{
+    public class ViewOrders
+    {
+        public IList<ClosedOrder> closedOrders { set; get; }
+        public IList<OpenOrder> openedOrders { set; get; }
+
+        public ViewOrders() 
+        {
+            closedOrders = new List<ClosedOrder>();
+            openedOrders = new List<OpenOrder>();
+        }
+    }
+}
