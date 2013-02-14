3e19d53db3fbac3d00299cd983bd92764a967dc3
Yann Perrin <perrin.yann@googlemail.com>
2013-02-13 22:06:44 -0500

    Created a security Service to encrypt strings

---
+++ Projects/Template/src/Service.Tests/SecurityServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using asi.asicentral.services;
+using asi.asicentral.interfaces;
+
+namespace asi.asicentral.Tests
+{
+    [TestClass]
+    public class SecurityServiceTest
+    {
+        [TestMethod]
+        public void EncryptDecryptTest()
+        {
+            ISecurityService securityService = new SecurityService();
+            string key = "My Key";
+            string test = "Test to encrypt";
+            string encryptedTest = securityService.Encrypt(key, test);
+            Assert.AreNotEqual(test, encryptedTest);
+            Assert.AreEqual(test, securityService.Decrypt(key, encryptedTest));
+            try
+            {
+                securityService.Decrypt(key + "1", encryptedTest);
+                Assert.IsTrue(false);
+            }
+            catch (Exception)
+            {
+                //expected
+            }
+            Assert.AreNotEqual(encryptedTest, securityService.Encrypt("other key", test));
+        }
+    }
+}
