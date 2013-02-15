a4691757fd0bc594e315c3a560e3ac9481f86d61
noh5017 <nhua@asicentral.com>
2013-02-15 10:59:46 -0500

    Collapsed expanding TD
    Fixed precision for decimal

--- Projects/ASI Admin/src/Web Application/Controllers/Store/ApplicationController.cs
+++ Projects/ASI Admin/src/Web Application/Controllers/Store/ApplicationController.cs
@@ -14,24 +14,39 @@ namespace asi.asicentral.web.Controllers.Store
         public IStoreService StoreObjectService { get; set; }
 
         [HttpGet]
-        public virtual ActionResult Edit(OrderDetail orderDetail)
+        public virtual ActionResult Edit(OrderDetailApplication orderDetailApplication)
         {
-            if (orderDetail == null)
-                throw new Exception("Parameter orderDetail cannot be null");
+            OrderDetailApplication application;
 
-            orderDetail = StoreObjectService.GetAll<OrderDetail>(true).Where(orderDtail => orderDtail.OrderId == orderDetail.OrderId && orderDtail.ProductId == orderDetail.ProductId).SingleOrDefault();
-            if (orderDetail == null)
-                throw new Exception("Invalid identifier for an order detail with id " + orderDetail.OrderId);
+            application = StoreObjectService.GetAll<SupplierMembershipApplication>(true).Where(theApp => theApp.Id == orderDetailApplication.Id).SingleOrDefault() as SupplierMembershipApplication;
+            if (application != null) return View("../Store/Application/Supplier", application);
 
-            OrderDetailApplication application = StoreObjectService.GetApplication(orderDetail);
-            if (application != null)
-            {
-                if (application is DistributorMembershipApplication) return View("../Store/Application/Distributor", application);
-                else if (application is SupplierMembershipApplication) return View("../Store/Application/Supplier", application);
-            }
+            application = StoreObjectService.GetAll<DistributorMembershipApplication>(true).Where(theApp => theApp.Id == orderDetailApplication.Id).SingleOrDefault() as DistributorMembershipApplication;
+            if (application != null) return View("../Store/Application/Distributor", application);
 
             // can't find any application - return nothing
             return new EmptyResult();
         }
+
+        //[HttpGet]
+        //public virtual ActionResult Edit(OrderDetail orderDetail)
+        //{
+        //    if (orderDetail == null)
+        //        throw new Exception("Parameter orderDetail cannot be null");
+
+        //    orderDetail = StoreObjectService.GetAll<OrderDetail>(true).Where(orderDtail => orderDtail.OrderId == orderDetail.OrderId && orderDtail.ProductId == orderDetail.ProductId).SingleOrDefault();
+        //    if (orderDetail == null)
+        //        throw new Exception("Invalid identifier for an order detail with id " + orderDetail.OrderId);
+
+        //    OrderDetailApplication application = StoreObjectService.GetApplication(orderDetail);
+        //    if (application != null)
+        //    {
+        //        if (application is DistributorMembershipApplication) return View("../Store/Application/Distributor", application);
+        //        else if (application is SupplierMembershipApplication) return View("../Store/Application/Supplier", application);
+        //    }
+
+        //    // can't find any application - return nothing
+        //    return new EmptyResult();
+        //}
     }
 }
