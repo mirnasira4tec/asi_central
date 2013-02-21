4e72080f93acbdbaa5d84562751242d4fadecd9c
noh5017 <nhua@asicentral.com>
2013-02-21 08:44:26 -0500

    Created some action methods for reject and accept
    created new viewmodels for order
    modified ui

--- Projects/ASI Admin/src/Web Application/Models/Store/ClosedOrder.cs
+++ Projects/ASI Admin/src/Web Application/Models/Store/ClosedOrder.cs
@@ -10,8 +10,40 @@ namespace asi.asicentral.web.Models.Store
 {
     public class ClosedOrder
     {
-        public OrderDetail orderDetail { set; get; }
+        private OrderDetail orderDetail;
+        
         public OrderDetailApplication Application { set; get; }
+        public String Email { set; get; }
+        public int Quantity
+        {
+            get
+            {
+                return orderDetail.Quantity.HasValue ? orderDetail.Quantity.Value : 0;
+            }
+        }
+
+        public int OrderId
+        {
+            get
+            {
+                return orderDetail.OrderId;
+            }
+        }
+        public String Item
+        {
+            get
+            {
+                return orderDetail.Product.Description;
+            }
+        }
+        public Decimal Price
+        {
+            get
+            {
+                if (this.orderDetail.Subtotal.HasValue) return Math.Round(this.orderDetail.Subtotal.Value, 2);
+                else return 0.00M;
+            }
+        }
         public String Billing
         {
             get
@@ -23,6 +55,11 @@ namespace asi.asicentral.web.Models.Store
                     orderDetail.Order.BillZip + ", " +
                     orderDetail.Order.BillCountry + ". Phone: " + orderDetail.Order.BillPhone;
             }
+        }
+
+        public void SetOrderDetail(OrderDetail orderDetail)
+        {
+            this.orderDetail = orderDetail;
         }
 
         public void SetApplicationFromService(IStoreService StoreService)
