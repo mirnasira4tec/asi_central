812705c4bbeb8b20735ec4659741196b3afa4a13
kphani <kphani@macrosoftindia.com>
2013-03-11 13:59:00 +0530

    Test commit, removed the added empty line

--- Projects/Template/src/Web Application/Controllers/HomeController.cs
+++ Projects/Template/src/Web Application/Controllers/HomeController.cs
@@ -12,7 +12,6 @@ namespace Web_Application.Controllers
         {
             ViewBag.Message = "Playing with the technology stack and good practices";
 
-
             return View("Index");
         }
     }
