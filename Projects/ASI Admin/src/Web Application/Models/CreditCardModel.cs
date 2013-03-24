1972abdad1fad0127dc5d0fac350cc0c4c99319e
Yann Perrin <perrin.yann@googlemail.com>
2013-03-24 10:01:24 -0400

    Added the code to test the Credit Card Web Service

---
+++ Projects/ASI Admin/src/Web Application/Models/CreditCardModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asi.asicentral.web.model
+{
+    public class CreditCardModel
+    {
+        public CreditCardModel()
+        {
+            SuccessMessages = new List<string>();
+            ErrorMessages = new List<string>();
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ServiceUrl { get; set; }
+        public IList<string> SuccessMessages { get; set; }
+        public IList<string> ErrorMessages { get; set; }
+    }
+}
