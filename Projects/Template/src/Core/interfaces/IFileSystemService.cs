a981236b83fbbf4daccc368a2c5e9fdf2aba3c07
Yann Perrin <perrin.yann@googlemail.com>
2013-03-29 14:50:53 -0400

    Created a new template interface for sending emails

---
+++ Projects/Template/src/Core/interfaces/IFileSystemService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asi.asicentral.interfaces
+{
+    public interface IFileSystemService
+    {
+        /// <summary>
+        /// Reads the content of a file
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        string ReadContent(string fileName);
+
+        /// <summary>
+        /// Checks if the file can be found
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        bool Exists(string fileName);
+    }
+}
---
+++ Projects/Template/src/Core/services/AssemblyFileService.cs
@@ -0,0 +1,41 @@
+﻿using asi.asicentral.interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asi.asicentral.services
+{
+    /// <summary>
+    /// Reads file contained within the assembly
+    /// </summary>
+    public class AssemblyFileService : IFileSystemService
+    {
+        private Assembly _baseAssembly;
+
+        public AssemblyFileService(Assembly assembly)
+        {
+            _baseAssembly = assembly;
+        }
+
+        public string ReadContent(string fileName)
+        {
+            string content = string.Empty;
+
+            using (Stream stream = _baseAssembly.GetManifestResourceStream(fileName))
+            using (TextReader reader = new StreamReader(stream))
+            {
+                content = reader.ReadToEnd();
+            }
+            return content;
+        }
+
+        public bool Exists(string fileName)
+        {
+            return _baseAssembly.GetManifestResourceNames().Contains(fileName);
+        }
+    }
+}
