9e2ea086fbe6df3e8ce9f1c98a99b0b5fed92428
Yann Perrin <perrin.yann@googlemail.com>
2013-05-10 17:03:01 -0400

    Creating api to access the media server

---
+++ Projects/ASI Admin/src/Web Application/Controllers/MediaController.cs
@@ -0,0 +1,39 @@
+﻿using asi.asicentral.util;
+using asi.asicentral.web.model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asi.asicentral.web.Controllers
+{
+    public class MediaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return List();
+        }
+
+        public ActionResult List(string path = null)
+        {
+            MediaFolderModel model = InitMediaFolder();
+            model.Path = string.IsNullOrEmpty(path) ? string.Empty : path;
+            model.URL = string.IsNullOrEmpty(path) ? model.BaseURL : model.BaseURL + path.Replace("\\", "/");;
+            model.Children = FileSystemHelper.GetFiles(model.BasePath + model.Path, model.BasePath);
+            return View("List", model);
+        }
+
+        private MediaFolderModel InitMediaFolder()
+        {
+            MediaFolderModel model = new MediaFolderModel();
+            model.BasePath = ConfigurationManager.AppSettings["MediaPath"];
+            model.BaseURL = ConfigurationManager.AppSettings["MediaURL"];
+            if (string.IsNullOrEmpty(model.BasePath) || string.IsNullOrEmpty(model.BaseURL)) throw new Exception("The media properties need to be setup");
+            if (!Directory.Exists(model.BasePath)) throw new Exception("The media properties seem to be incorrect");
+            return model;
+        }
+    }
+}
---
+++ Projects/ASI Admin/src/Web Application/Models/MediaFolderModel.cs
@@ -0,0 +1,17 @@
+﻿using asi.asicentral.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asi.asicentral.web.model
+{
+    public class MediaFolderModel
+    {
+        public string BasePath { get; set; }
+        public string BaseURL { get; set; }
+        public string Path { get; set; }
+        public string URL { get; set; }
+        public IList<FileModel> Children { get; set; }
+    }
+}
