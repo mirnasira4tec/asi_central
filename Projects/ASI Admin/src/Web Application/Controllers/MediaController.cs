c1c5e5599c2e273f49ade0f1237c1bca9fcd504b
Yann Perrin <perrin.yann@googlemail.com>
2013-05-13 14:35:20 -0400

    Removed media url

--- Projects/ASI Admin/src/Web Application/Controllers/MediaController.cs
+++ Projects/ASI Admin/src/Web Application/Controllers/MediaController.cs
@@ -30,8 +30,7 @@ namespace asi.asicentral.web.Controllers
         {
             MediaFolderModel model = new MediaFolderModel();
             model.BasePath = ConfigurationManager.AppSettings["MediaPath"];
-            model.BaseURL = ConfigurationManager.AppSettings["MediaURL"];
-            if (string.IsNullOrEmpty(model.BasePath) || string.IsNullOrEmpty(model.BaseURL)) throw new Exception("The media properties need to be setup");
+            if (string.IsNullOrEmpty(model.BasePath)) throw new Exception("The media properties need to be setup");
             if (!Directory.Exists(model.BasePath)) throw new Exception("The media properties seem to be incorrect");
             return model;
         }
