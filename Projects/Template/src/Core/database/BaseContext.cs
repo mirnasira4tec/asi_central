66327e9de4f277632318aebd51c9ca708bd540b7
Yann Perrin <perrin.yann@googlemail.com>
2013-05-18 00:47:09 -0400

    Created ability to turn on SQL tracing using the EFTracing Nuget package

--- Projects/Template/src/Core/database/BaseContext.cs
+++ Projects/Template/src/Core/database/BaseContext.cs
@@ -1,7 +1,12 @@
 ﻿using asi.asicentral.interfaces;
+using asi.asicentral.services;
+using EFTracingProvider;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -12,8 +17,10 @@ namespace asi.asicentral.database
     public abstract class BaseContext : DbContext, IValidatedContext
     {
         private IDictionary<Type, PropertyInfo> _supportedTypes;
+        private ILogService _log;
 
-        public BaseContext(string connectionString) : base(connectionString)
+        public BaseContext(string connectionString)
+            : base(connectionString)
         {
             if (_supportedTypes == null)
             {
@@ -37,6 +44,33 @@ namespace asi.asicentral.database
                 }
                 _supportedTypes = supportedTypes;
             }
+        }
+
+        public void EnableTracing(Type type)
+        {
+            _log = LogService.GetLog(type);
+            // enable sql tracing
+            ((IObjectContextAdapter)this).ObjectContext.EnableTracing();
+            IEnumerable<EFTracingConnection> traceConnections = ((IObjectContextAdapter)this).ObjectContext.Connection.GetTracingConnections();
+            List<EFTracingConnection> connectionList = new List<EFTracingConnection>(traceConnections);
+            connectionList.ForEach(
+                c =>
+                {
+                    c.CommandExecuting += (s, e) => LogSql(0, e);
+                    c.CommandFinished += (s, e) => LogSql(1, e);
+                    c.CommandFailed += (s, e) => LogSql(2, e);
+                });
+        }
+
+        /// <summary>
+        /// The logging method.  Overwrite this method to change the logging strategy.
+        /// </summary>
+        /// <param name="e">The DB command event.</param>
+        protected void LogSql(int type, CommandExecutionEventArgs e)
+        {
+            if (type == 0) _log.Debug("Starting a SQL Execution");
+            else if (type == 1) _log.Debug("Executed SQL (" + e.Duration + "): " + e.ToTraceString());
+            else _log.Error("Failed to execute SQL: " + e.ToTraceString());
         }
 
         #region IValidatedContext
@@ -72,6 +106,52 @@ namespace asi.asicentral.database
                 _supportedTypes.Clear();
                 _supportedTypes = null;
             }
+        }
+
+        #endregion
+
+        #region tracing supporting methods
+
+        private static DbConnection CreateTracingConnection(string nameOrConnectionString)
+        {
+            try
+            {
+                // this only supports entity connection strings http://msdn.microsoft.com/en-us/library/cc716756.aspx
+                return EFTracingProviderUtils.CreateTracedEntityConnection(nameOrConnectionString);
+            }
+            catch (ArgumentException)
+            {
+                // an invalid entity connection string is assumed to be a normal connection string name or connection string (Code First)
+                ConnectionStringSettings connectionStringSetting = ConfigurationManager.ConnectionStrings[nameOrConnectionString.Replace("name=", "")];
+                string connectionString;
+                string providerName;
+
+                if (connectionStringSetting != null)
+                {
+                    connectionString = connectionStringSetting.ConnectionString;
+                    providerName = connectionStringSetting.ProviderName;
+                }
+                else
+                {
+                    providerName = "System.Data.SqlClient";
+                    connectionString = nameOrConnectionString;
+                }
+
+                return CreateTracingConnection(connectionString, providerName);
+            }
+        }
+
+        private static EFTracingConnection CreateTracingConnection(string connectionString, string providerInvariantName)
+        {
+            string wrapperConnectionString =
+                String.Format(@"wrappedProvider={0};{1}", providerInvariantName, connectionString);
+
+            EFTracingConnection connection =
+                new EFTracingConnection
+                {
+                    ConnectionString = wrapperConnectionString
+                };
+            return connection;
         }
 
         #endregion
