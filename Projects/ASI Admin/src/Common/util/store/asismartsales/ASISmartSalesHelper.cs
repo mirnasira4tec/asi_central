398617ac169da53c590277f4c98098e857e5ec3e
kphani <kphani@macrosoftindia.com>
2013-09-26 10:35:16 +0530

    ASCN-359: ASI Store - ASI SmartSales - Customize & Buy wrong packages

--- Projects/ASI Admin/src/Common/util/store/asismartsales/ASISmartSalesHelper.cs
+++ Projects/ASI Admin/src/Common/util/store/asismartsales/ASISmartSalesHelper.cs
@@ -9,15 +9,22 @@ namespace asi.asicentral.util.store
 {
     public class ASISmartSalesHelper
     {
+        public static readonly decimal[] DISTRIBUTOR_MAGAZINE_PRODUCT_IDS = { 35, 35, 30, 25, 35 };
+
         public static IList<SelectListItem> GetCurrentMembershipOptions(string value = null)
         {
             IList<SelectListItem> membershipOptions = new List<SelectListItem>();
-            membershipOptions.Add(new SelectListItem() { Text = Resource.Basic, Value = "35", Selected = ("35" == value) });
-            membershipOptions.Add(new SelectListItem() { Text = Resource.Standard, Value = "30", Selected = ("30" == value) });
-            membershipOptions.Add(new SelectListItem() { Text = Resource.Executive, Value = "25", Selected = ("25" == value) });
-            membershipOptions.Add(new SelectListItem() { Text = Resource.Professional, Value = "35", Selected = ("35" == value) });
-            membershipOptions.Add(new SelectListItem() { Text = Resource.NA, Value = "35", Selected = ("35" == value) });
+            membershipOptions.Add(new SelectListItem() { Text = Resource.Membership, Value = "0", Selected = ("0" == value) });
+            membershipOptions.Add(new SelectListItem() { Text = Resource.Basic, Value = "1", Selected = ("1" == value) });
+            membershipOptions.Add(new SelectListItem() { Text = Resource.Standard, Value = "2", Selected = ("2" == value) });
+            membershipOptions.Add(new SelectListItem() { Text = Resource.Executive, Value = "3", Selected = ("3" == value) });
+            membershipOptions.Add(new SelectListItem() { Text = Resource.NA, Value = "4", Selected = ("4" == value) });
             return membershipOptions;
         }
+
+        public static decimal GetCost(int optionId)
+        {
+            return DISTRIBUTOR_MAGAZINE_PRODUCT_IDS[optionId];
+         }
     }
 }
