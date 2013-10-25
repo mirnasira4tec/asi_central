a655799549b29863b89dedb8edab11721669d067
khawaja83 <khawajausmannadeem@gmail.com>
2013-10-25 21:56:17 +0500

    Email Express Admin Part

---
+++ Projects/ASI Admin/src/Web Application/Models/Store/Order/EmailExpressModel.cs
@@ -0,0 +1,167 @@
+﻿using asi.asicentral.interfaces;
+using asi.asicentral.model.store;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace asi.asicentral.web.model.store
+{
+    public class EmailExpressModel : IMembershipModel
+    {
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "CompanyName")]
+        public string Company { get; set; }
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Street1")]
+        public string Address1 { get; set; }
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Street2")]
+        public string Address2 { get; set; }
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "City")]
+        public string City { get; set; }
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Zipcode")]
+        public string Zip { get; set; }
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "State")]
+        public string State { get; set; }
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Country")]
+        public string Country { get; set; }
+        [RegularExpression(@"^(?=[^0-9]*[0-9])[0-9\s!@#$%^&*()_\-+]+$", ErrorMessageResourceName = "FieldInvalidNumber", ErrorMessageResourceType = typeof(asi.asicentral.Resource))]
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Phone")]
+        public string Phone { get; set; }
+        public string InternationalPhone { get; set; }
+        public bool HasShipAddress { get; set; }
+        public bool HasBillAddress { get; set; }
+
+        #region Billing information
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "BillingTollPhone")]
+        public string BillingTollFree { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Fax")]
+        [RegularExpression(@"^(?=[^0-9]*[0-9])[0-9\s!@#$%^&*()_\-+]+$", ErrorMessageResourceName = "FieldInvalidNumber", ErrorMessageResourceType = typeof(asi.asicentral.Resource))]
+        public string BillingFax { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Street1")]
+        public string BillingAddress1 { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Street2")]
+        public string BillingAddress2 { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "City")]
+        public string BillingCity { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "State")]
+        public string BillingState { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Zipcode")]
+        public string BillingZip { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Country")]
+        public string BillingCountry { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Phone")]
+        [RegularExpression(@"^(?=[^0-9]*[0-9])[0-9\s!@#$%^&*()_\-+]+$", ErrorMessageResourceName = "FieldInvalidNumber", ErrorMessageResourceType = typeof(asi.asicentral.Resource))]
+        public string BillingPhone { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string BillingEmail { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "WebUrl")]
+        [DataType(DataType.Url)]
+        public string BillingWebUrl { get; set; }
+
+        #endregion Billing information
+
+        #region shipping information
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "ShippingAddress")]
+        public string ShippingStreet1 { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "ShippingAddress2")]
+        public string ShippingStreet2 { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "ShippingCity")]
+        public string ShippingCity { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "ShippingState")]
+        public string ShippingState { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "ShippingZip")]
+        public string ShippingZip { get; set; }
+
+        [Display(ResourceType = typeof(asi.asicentral.Resource), Name = "Country")]
+        public string ShippingCountry { get; set; }
+
+        #endregion shipping information
+
+        #region Email Express information
+
+        public int ItemTypeId { get; set; }
+        public string Dates { get; set; }
+        public decimal TotalCost { get; set; }
+        public List<System.Web.Mvc.SelectListItem> ItemTypes { get { return asi.asicentral.util.store.EmailExpressHelper.GetItemTypeOptions(); } }
+        
+        #endregion Email Express information
+
+        public int OrderId { get; set; }
+        public int OrderDetailId { get; set; }
+        public StoreIndividual BillingIndividual { get; set; }
+        public string ActionName { get; set; }
+        public string ExternalReference { get; set; }
+        public bool IsCompleted { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public string ProductName { get; set; }
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public IList<StoreDetailEmailExpressItem> Videos { get; set; }
+        public IList<StoreIndividual> Contacts { get; set; }
+
+        /// <summary>
+        /// Required for MVC to rebuild the model
+        /// </summary>
+        /// 
+        public EmailExpressModel()
+            : base()
+        {
+            this.Contacts = new List<StoreIndividual>();
+        }
+
+        public EmailExpressModel(StoreOrderDetail orderdetail, StoreDetailEmailExpress emailepxress, IStoreService storeService)
+            : base()
+        {
+            this.Contacts = new List<StoreIndividual>();
+            StoreOrder order = orderdetail.Order;
+            BillingIndividual = order.BillingIndividual;
+            OrderDetailId = orderdetail.Id;
+
+            ActionName = "Approve";
+            ExternalReference = order.ExternalReference;
+            if (orderdetail.Product != null)
+            {
+                ProductName = orderdetail.Product.Name;
+                ProductId = orderdetail.Product.Id;
+            }
+
+            #region Fill Email Express data
+
+            StoreDetailEmailExpress item = storeService.GetAll<StoreDetailEmailExpress>().Where(model => model.OrderDetailId == OrderDetailId).SingleOrDefault();
+            if (item != null)
+            {
+                this.ItemTypeId = item.ItemTypeId;
+                this.TotalCost = orderdetail.Quantity * orderdetail.Cost;
+                string Dates = string.Empty;
+                List<StoreDetailEmailExpressItem> items = storeService.GetAll<StoreDetailEmailExpressItem>().Where(model => model.OrderDetailId == OrderDetailId).OrderBy(model => model.Sequence).ToList();
+                foreach (var dateitem in items)
+                    Dates += dateitem.AdSelectedDate.Date.ToString("ddd, dd MMM yy") + "\r\n";
+                this.Dates = Dates;
+            }
+            #endregion
+
+            OrderId = order.Id;
+            Price = order.Total;
+            OrderStatus = order.ProcessStatus;
+            IsCompleted = order.IsCompleted;
+            MembershipModelHelper.PopulateModel(this, order);
+        }
+    }
+}
