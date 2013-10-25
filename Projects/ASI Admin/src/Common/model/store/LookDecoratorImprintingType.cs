88b46c6d7ab9ef3a483571dc5eebdf9519806e85
kphani <kphani@macrosoftindia.com>
2013-10-25 12:40:22 +0530

    Added constants for Imprint Types as in Supplier

--- Projects/ASI Admin/src/Common/model/store/LookDecoratorImprintingType.cs
+++ Projects/ASI Admin/src/Common/model/store/LookDecoratorImprintingType.cs
@@ -8,6 +8,15 @@ namespace asi.asicentral.model.store
 {
     public class LookDecoratorImprintingType
     {
+        public const string IMPRINT_EMBROIDERY = "Embroidery";
+        public const string IMPRINT_SCREENPRINTING = "Screen Printing";
+        public const string IMPRINT_HEATTRANSFER = "Heat Transfer";
+        public const string IMPRINT_DIGITIZING = "Digitizing";
+        public const string IMPRINT_ENGRAVING = "Engraving";
+        public const string IMPRINT_SUBLIMINATION = "Sublimation";
+        public const string IMPRINT_MONOGRAMMING = "Monogramming";
+        public const string IMPRINT_OTHER = "Other";
+
         public int Id { get; set; }
         public string Description { get; set; }
         public string SubCode { get; set; }
