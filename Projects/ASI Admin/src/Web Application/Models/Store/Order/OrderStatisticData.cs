7d08ac175fb0eef71e1348d6d624eb6a6e182a04
Yann Perrin <perrin.yann@googlemail.com>
2013-11-05 00:08:42 -0500

    Created the data for products through the store

--- Projects/ASI Admin/src/Web Application/Models/Store/Order/OrderStatisticData.cs
+++ Projects/ASI Admin/src/Web Application/Models/Store/Order/OrderStatisticData.cs
@@ -15,7 +15,6 @@ namespace asi.asicentral.web.model.store.order
         public decimal TotalAmount { get; set; }
         public string Message { set; get; }
     }
-
     public class GroupedData
     {
         public string GroupName { get; set; }
@@ -28,5 +27,36 @@ namespace asi.asicentral.web.model.store.order
         public decimal? AmountRejected { set; get; }
         public int CountApproved { get; set; }
         public decimal? AmountApproved { set; get; }
+    }
+    public class ProductStatisticData
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public IList<Group> Data { get; set; }
+        public string Message { set; get; }
+    }
+    public class Group
+    {
+        public Group()
+        {
+            Data = new GroupData[8];
+            Data[0] = new GroupData() { Name = "Clicked on the link Only", Amount = 0, Count = 0 };
+            Data[1] = new GroupData() { Name = "Selected a product", Amount = 0, Count = 0 };
+            Data[2] = new GroupData() { Name = "Entered Company information", Amount = 0, Count = 0 };
+            Data[3] = new GroupData() { Name = "Entered billing/shipping information", Amount = 0, Count = 0 };
+            Data[4] = new GroupData() { Name = "Confirmed the order", Amount = 0, Count = 0 };
+            Data[5] = new GroupData() { Name = "Rejected", Amount = 0, Count = 0 };
+            Data[6] = new GroupData() { Name = "Pending Approval", Amount = 0, Count = 0 };
+            Data[7] = new GroupData() { Name = "Total", Amount = 0, Count = 0 };
+        }
+        public string Name { get; set; }
+        public GroupData[] Data { get; set; }
+    }
+    public class GroupData
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public decimal Amount { get; set; }
+        public decimal AnnualizedAmount { get; set; }
     }
 }
