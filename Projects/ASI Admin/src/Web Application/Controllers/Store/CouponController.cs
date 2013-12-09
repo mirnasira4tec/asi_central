cf3d9414dc3946bae6c0d08f54c32b1bd60704fd
unknown <sailekshmy@M182.macrosoftindia.com>
2013-12-09 12:44:07 +0530

    Created coupon tab

---
+++ Projects/ASI Admin/src/Web Application/Controllers/Store/CouponController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asi.asicentral.web.Controllers.Store
+{
+    public class CouponController : Controller
+    {
+        //
+        // GET: /Coupon/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View("../Store/Admin/Coupon");
+        }
+
+    }
+}
