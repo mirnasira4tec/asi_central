00ab3fa860e360e51c8303606a72ab07360eedc6
unknown <sailekshmy@M182.macrosoftindia.com>
2013-12-17 18:14:03 +0530

    Helper class added for Common store

---
+++ Projects/ASI Admin/src/Common/util/store/commonstore/CommonStoreHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using asi.asicentral.Resources;
+
+namespace asi.asicentral.util.store
+{
+    public class CommonStoreHelper
+    {
+        public static readonly decimal[] CommonStore_Cost = {29,99} ;
+        
+    }
+}
