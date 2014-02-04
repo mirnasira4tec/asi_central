ee231c5bcafc9adf017c6da7952e4aedaf77e57d
Xinyu <vt3522@yahoo.com>
2014-02-04 08:48:58 -0500

    ASCN-731 : We need to be able to purchase magazine advertising through the store

--- Projects/ASI Admin/src/Common/model/store/LookAdSize.cs
+++ Projects/ASI Admin/src/Common/model/store/LookAdSize.cs
@@ -9,15 +9,14 @@ namespace asi.asicentral.model.store
 
     public class LookAdSize
     {
-
         public int Id { get; set; }
-
         public MagazineType MagazineId { get; set; }
-
         public string Description { get; set; }
-
         public string Code { get; set; }
-
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string UpdateSource { get; set; }
+        
         public bool Equals(LookAdSize o)
         {
             bool result = false;
--- Projects/ASI Admin/src/Common/model/store/LookMagazineIssue.cs
+++ Projects/ASI Admin/src/Common/model/store/LookMagazineIssue.cs
@@ -6,19 +6,20 @@ using System.Threading.Tasks;
 
 namespace asi.asicentral.model.store
 {
-
     public class LookMagazineIssue
     {
-
         public int Id { get; set; }
-
         public MagazineType ProductId { get; set; }
-
         public DateTime Issue { get; set; }
-
+        public DateTime ReservationDeadline { get; set; }
+        public DateTime MaterialDeadline { get; set; }
+        public DateTime MailingDate { get; set; }
         public bool IsChecked { get; set; }
-
-        public bool Equals(LookMagazineIssue o)
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string UpdateSource { get; set; }
+        
+         public bool Equals(LookAdSize o)
         {
             bool result = false;
             if ((object)o != null)
