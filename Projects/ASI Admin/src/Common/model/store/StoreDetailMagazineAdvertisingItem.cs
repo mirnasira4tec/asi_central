1988aae797bf3e50f6e8e12c1f9cc41ed6f0476f
sai <sailekshmy@macrosoftindia.com>
2014-02-04 14:35:55 +0530

    change for ASCN-777
    and
    adding virtual keyword to variables.

--- Projects/ASI Admin/src/Common/model/store/StoreDetailMagazineAdvertisingItem.cs
+++ Projects/ASI Admin/src/Common/model/store/StoreDetailMagazineAdvertisingItem.cs
@@ -11,9 +11,9 @@ namespace asi.asicentral.model.store
     {
         public int Id { get; set; }
         public int OrderDetailId { get; set; }
-        public LookMagazineIssue Issue { get; set; }
-        public LookAdSize Size { get; set; }
-        public LookAdPosition Position { get; set; }
+        public virtual LookMagazineIssue Issue { get; set; }
+        public virtual LookAdSize Size { get; set; }
+        public virtual LookAdPosition Position { get; set; }
         public bool ArtWork { get; set; }
         public int Sequence { get; set; }
         public DateTime CreateDate { get; set; }
