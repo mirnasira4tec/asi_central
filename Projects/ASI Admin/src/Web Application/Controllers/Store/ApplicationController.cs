d8bfb0addb341eed1467a5ac557f17f8dfdf387c
sai <sailekshmy@macrosoftindia.com>
2014-02-13 19:45:39 +0530

    added test cases and checking null reference on approved by

--- Projects/ASI Admin/src/Web Application/Controllers/Store/ApplicationController.cs
+++ Projects/ASI Admin/src/Web Application/Controllers/Store/ApplicationController.cs
@@ -826,8 +826,11 @@ namespace asi.asicentral.web.Controllers.Store
                 fulfilmentService.Process(order, application);
                 order.ProcessStatus = OrderStatus.Approved;
                 order.ApprovedDate = DateTime.UtcNow;
-                if (System.Web.HttpContext.Current.User.Identity as System.Security.Principal.WindowsIdentity != null)
-                    order.ApprovedBy = ((System.Security.Principal.WindowsIdentity)System.Web.HttpContext.Current.User.Identity).Name;
+                if (System.Web.HttpContext.Current != null)
+                {
+                    if (System.Web.HttpContext.Current.User.Identity as System.Security.Principal.WindowsIdentity != null)
+                        order.ApprovedBy = ((System.Security.Principal.WindowsIdentity)System.Web.HttpContext.Current.User.Identity).Name;
+                }
             }
             else if (command == ApplicationController.COMMAND_REJECT)
             {
