85b9f779c66afdfe8c31a2d337a43b5c9efb38c0
kphani <kphani@macrosoftindia.com>
2014-03-26 13:46:34 +0530

    ASCN-902: Make sure the login for store is using OAuth.asicentral.com

--- Projects/Template/src/Core/OAuth/ASIOAuthClient.cs
+++ Projects/Template/src/Core/OAuth/ASIOAuthClient.cs
@@ -11,18 +11,72 @@ namespace asi.asicentral.oauth
 {
     public class ASIOAuthClient : AspNetClient
     {
-        public static User _user { get; set; }
-        public static bool IsAuthenticatedUser { get; set; }
+        private static Jade.UserManagement.User _juser { get; set; }
+        private static Jade.UserManagement.User JUser
+        {
+            get
+            {
+                if (_juser == null)
+                {
+                    _juser = new Jade.UserManagement.User();
+                    return _juser;
+                }
+                else { return _juser; }
+            }
+        }
         public ASIOAuthClient(string clientIdentifier, string clientSecret, System.Uri authorizationEndpoint, System.Uri tokenEndpoint, System.Uri apiEndpoint)
             : base(clientIdentifier, clientSecret, authorizationEndpoint, tokenEndpoint, apiEndpoint)
         {
             
         }
 
-        public Jade.UserManagement.DataObjects.User GetUser(int sso)
+        public static asi.asicentral.model.User GetUser(int sso)
         {
-            if (_user == null) _user = new User();
-            Jade.UserManagement.DataObjects.User user = _user.Get(sso);
+            model.User user = null;
+            Jade.UserManagement.DataObjects.User jadeuser = JUser.Get(sso);
+            
+            if (jadeuser != null)
+            {
+                user = new model.User();
+                user.SSOId  = jadeuser.SSOId;
+                user.Email  = jadeuser.Email;
+                user.UserName  = jadeuser.UserName;
+                user.InternalUserId  = jadeuser.InternalUserId;
+                user.IndividualId  = jadeuser.IndividualId;
+                user.Password  = jadeuser.Password;
+                user.FirstName  = jadeuser.FirstName;
+                user.MiddleName  = jadeuser.MiddleName;
+                user.LastName  = jadeuser.LastName;
+                user.Prefix  = jadeuser.Prefix;
+                user.Suffix  = jadeuser.Suffix;
+                user.CreateDate  = jadeuser.CreateDate;
+                user.UpdateDate  = jadeuser.UpdateDate;
+                user.UpdateSource  = jadeuser.UpdateSource;
+                user.CompanyId  = jadeuser.CompanyId;
+                user.PasswordHint  = jadeuser.PasswordHint;
+                user.PasswordAnswer  = jadeuser.PasswordAnswer;
+                user.PasswordQuestionCode  = jadeuser.PasswordQuestionCode;
+                user.PasswordQuestion  = jadeuser.PasswordQuestion;
+                user.IsTelephoneUpdatesAllowed  = jadeuser.IsTelephoneUpdatesAllowed;
+                user.TelephonePassword  = jadeuser.TelephonePassword;
+                user.PasswordResetRequired  = jadeuser.PasswordResetRequired;
+                user.PasswordResetKey  = jadeuser.PasswordResetKey;
+                user.PasswordResetExpireDate  = jadeuser.PasswordResetExpireDate;
+                user.TerminatedDate  = jadeuser.TerminatedDate;
+                user.IsSalesRep  = jadeuser.IsSalesRep;
+                user.IsPVAdmin  = jadeuser.IsPVAdmin;
+                user.IsConnectPrimary  = jadeuser.IsConnectPrimary;
+                user.StatusCode  = jadeuser.StatusCode;
+                user.SignonTypeCode  = jadeuser.SignonTypeCode;
+                user.MmsLink  = jadeuser.MmsLink;
+                user.AsiNumber  = jadeuser.AsiNumber;
+                user.CompanyName  = jadeuser.CompanyName;
+                user.MemberStatus_CD  = jadeuser.MemberStatus_CD;
+                user.MemberType_CD  = jadeuser.MemberType_CD;
+                user.Phone  = jadeuser.Phone;
+                user.Cell  = jadeuser.Cell;
+                user.Fax = jadeuser.Fax;
+            }
             return user;
         }
     }
---
+++ Projects/Template/src/Core/model/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asi.asicentral.model
+{
+    public class User
+    {
+        public int SSOId { get; set; }
+
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public Nullable<int> InternalUserId { get; set; }
+        public Nullable<int> IndividualId { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Prefix { get; set; }
+        public string Suffix { get; set; }
+        public System.DateTime CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public string UpdateSource { get; set; }
+        public int CompanyId { get; set; }
+        public string PasswordHint { get; set; }
+        public string PasswordAnswer { get; set; }
+        public string PasswordQuestionCode { get; set; }
+        public string PasswordQuestion { get; set; }
+        public int IsTelephoneUpdatesAllowed { get; set; }
+        public string TelephonePassword { get; set; }
+        public int PasswordResetRequired { get; set; }
+        public string PasswordResetKey { get; set; }
+        public Nullable<System.DateTime> PasswordResetExpireDate { get; set; }
+        public Nullable<System.DateTime> TerminatedDate { get; set; }
+        public int IsSalesRep { get; set; }
+        public int IsPVAdmin { get; set; }
+        public int IsConnectPrimary { get; set; }
+        public string StatusCode { get; set; }
+        public string SignonTypeCode { get; set; }
+
+        public string MmsLink { get; set; }
+
+        public string AsiNumber { get; set; }
+        public string CompanyName { get; set; }
+        public string MemberStatus_CD { get; set; }
+        public string MemberType_CD { get; set; }
+
+        public string Phone { get; set; }
+        public string Cell { get; set; }
+        public string Fax { get; set; }
+    }
+}
