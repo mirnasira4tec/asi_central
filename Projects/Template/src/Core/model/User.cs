834afdcc397593e8817b5b6df369566536ec438c
kphani <kphani@macrosoftindia.com>
2014-04-04 16:19:00 +0530

    ASCN-902: Added properties required to store address
    Added required methods to create ASI Client object with Jade

--- Projects/Template/src/Core/model/User.cs
+++ Projects/Template/src/Core/model/User.cs
@@ -50,5 +50,14 @@ namespace asi.asicentral.model
         public string Phone { get; set; }
         public string Cell { get; set; }
         public string Fax { get; set; }
+
+        //Added more for address
+        public string Title { get; set; }
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
+        public string Zip { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
     }
 }
