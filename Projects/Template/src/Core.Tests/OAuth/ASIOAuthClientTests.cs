c803c355545f643fd2f6d5ddbc1a920a031b8401
kphani <kphani@macrosoftindia.com>
2014-04-25 11:58:34 +0530

    Added test cases to validate user by email and username, password

--- Projects/Template/src/Core.Tests/OAuth/ASIOAuthClientTests.cs
+++ Projects/Template/src/Core.Tests/OAuth/ASIOAuthClientTests.cs
@@ -8,10 +8,31 @@ namespace Core.Tests.OAuth
     public class ASIOAuthClientTests
     {
         [TestMethod]
-        public void IsValidUser()
+        public void IsValidUserByEmail()
         {
             bool result = ASIOAuthClient.IsValidUser("kphani@macrosoftindia.com");
             Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsValidUserByFalseEmail()
+        {
+            bool result = ASIOAuthClient.IsValidUser("kphani1@macrosoftindia.com");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsValidUserByCredentials()
+        {
+            bool result = ASIOAuthClient.IsValidUser("125724pk", "password1");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsValidUserByFalseCredentials()
+        {
+            bool result = ASIOAuthClient.IsValidUser("125724pk1", "password1");
+            Assert.IsFalse(result);
         }
     }
 }
