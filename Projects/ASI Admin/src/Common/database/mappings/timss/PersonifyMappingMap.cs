6a3623d3532fa8a44a74c27eec00dc9b8118bc61
yperrin <yperrin@asicentral.com>
2014-05-31 15:34:52 -0400

    Creating model and database classes for personify mapping table

---
+++ Projects/ASI Admin/src/Common/database/mappings/timss/PersonifyMappingMap.cs
@@ -0,0 +1,59 @@
+﻿using asi.asicentral.model.timss;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asi.asicentral.database.mappings.timss
+{
+    public class PersonifyMappingMap : EntityTypeConfiguration<PersonifyMapping>
+    {
+        public PersonifyMappingMap()
+        {
+            this.ToTable("PERS-PRODUCT-MAPPING");
+            this.HasKey(t => new { t.Identifier });
+
+            // Properties
+            this.Property(t => t.Identifier)
+                .HasColumnName("Identifier")
+                .IsRequired();
+
+            this.Property(t => t.StoreContext)
+                .HasColumnName("Store_Context");
+
+            this.Property(t => t.StoreProduct)
+                .HasColumnName("Store_Product")
+                .IsRequired();
+
+            this.Property(t => t.StoreOption)
+                .HasColumnName("Store_Option")
+                .IsRequired();
+
+            this.Property(t => t.PersonifyProduct)
+                .HasColumnName("Pers_Product")
+                .IsRequired();
+
+            this.Property(t => t.PersonifyRateCode)
+                .HasColumnName("Pers_RateCode")
+                .IsRequired();
+
+            this.Property(t => t.PersonifyRateStructure)
+                .HasColumnName("Pers_RateStructure")
+                .IsRequired();
+
+            this.Property(t => t.CreateDateUTC)
+                .HasColumnName("CreateDateUTC")
+                .IsRequired();
+
+            this.Property(t => t.UpdateDateUTC)
+                .HasColumnName("UpdateDateUTC")
+                .IsRequired();
+
+            this.Property(t => t.UpdateSource)
+                .HasColumnName("UpdateSource")
+                .IsRequired();
+        }
+    }
+}
---
+++ Projects/ASI Admin/src/Common/model/timss/PersonifyMapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asi.asicentral.model.timss
+{
+    public class PersonifyMapping
+    {
+        public Guid Identifier { get; set; }
+        public int? StoreContext { get; set; }
+        public int StoreProduct { get; set; }
+        public string StoreOption { get; set; }
+        public int PersonifyProduct { get; set; }
+        public string PersonifyRateCode { get; set; }
+        public string PersonifyRateStructure { get; set; }
+        public DateTime CreateDateUTC { get; set; }
+        public DateTime UpdateDateUTC { get; set; }
+        public string UpdateSource { get; set; }
+
+    }
+}
