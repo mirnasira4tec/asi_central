7df1380a9af7c1bd05ad4f26999e3ab0f3afb597
sai1234 <sailekshmy@macrosoftindia.com>
2014-07-03 07:42:44 -0400

    ASI Store Admin-Approve button click error and PFP error

---
+++ Projects/ASI Admin/src/Common/util/CommonUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asi.asicentral.util
+{
+   public class CommonUtil
+    {
+       public static Guid GuidCreator(string id)
+       {
+           StringBuilder a = new StringBuilder();
+           int rem = id.Length - 32;
+           for (int i = 0; i < Math.Abs(rem); i++)
+           {
+               a.Append("0");
+           }
+           a.Append(id);
+           Guid extRef = new Guid(a.ToString());
+           return extRef;
+       }
+    }
+}
