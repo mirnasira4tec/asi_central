a9da9003fd16fc5e4446944c821924a9bc83225d
rrenjith <rrenjith@macrosoftindia.com>
2014-08-20 10:10:47 -0400

    Replacing Old banner for Promogram and code cleanup and optimization

--- Projects/DB Conversions/src/DBConversions/Menu.cs
+++ Projects/DB Conversions/src/DBConversions/Menu.cs
@@ -24,8 +24,9 @@ namespace DBConversions
         public static readonly string FOLDER_DOC_TYPE = "issue";
         public static readonly string VOLUME_DOC_TYPE = "NewsItem";
         public static readonly int TEMPLATE_ID=1116;
-        public static readonly  Regex regex = new Regex(@"(?<info>.+)\s*(?<date>\d\d/\d+/\d+)");
-        public IContentService service = null;
+        public static readonly  Regex Regex = new Regex(@"(?<info>.+)\s*(?<date>\d\d/\d+/\d+)");
+        public IContentService Service = null;
+       // public Dictionary<int, string> Childnodesdictionary = new Dictionary<int, string>();
         public Dictionary<int, int> nodeFolderIds = new Dictionary<int, int>()
             {
                 {3227,372},
@@ -37,6 +38,7 @@ namespace DBConversions
                 {3233,551},
                 {3234,552}
             };
+        
         public Menu()
         {
             InitializeComponent();
@@ -48,133 +50,124 @@ namespace DBConversions
             
             List<int> parentNodeID = new List<int>();
            
-            if (service == null)
+            if (Service == null)
             {
-                service = getContentservice();
+                Service = getContentservice();
             }
             foreach (KeyValuePair<int, int> node in nodeFolderIds)
             {
                 List<NewsLetter> Newsletters = new List<NewsLetter>();
-                string sql = String.Format("SELECT  content_title,content_html,date_created from asicentral..[content] where folder_ID in ('{0}')  order by date_created desc ", node.Value);
-               
+                string sql = String.Format("SELECT content_title,content_html,date_created from asicentral..[content] where folder_ID in ('{0}')  order by date_created desc ", node.Value);
                 Newsletters = RetriveDatabaseValues(sql);
                 CreateNodes(node, Newsletters);
             }
             lbl_confirmation.Text = "Nodes creation completed!";
         }
 
+        
 
         internal void CreateNodes(KeyValuePair<int, int> node, List<NewsLetter> Newsletternodes)
         {
            
             string currentnodeYear = string.Empty;
             int parentContentID = -1;
-             
-
             #region code to insert in to umbraco DB
             foreach (NewsLetter newsletterobj in Newsletternodes)
             {
-                string Issuedyear = newsletterobj.date.ToString("Y").Replace(',', ' ');
-                DateTime monthissued = DateTime.Parse(string.Concat("01 ", Issuedyear));
-                
-                IContent yearContent = null;
-                
+                string issuedYear = newsletterobj.date.ToString("Y").Replace(',', ' ');
+                DateTime monthIssued = DateTime.Parse(string.Concat("01 ", issuedYear));
 
-                if (currentnodeYear != Issuedyear)
+                IContent yearContent = null;
+
+
+                if (currentnodeYear != issuedYear)
                 {
                     parentContentID = -1;
-                    yearContent = service.CreateContent(Issuedyear.ToString(), node.Key, FOLDER_DOC_TYPE);
-                    var rootnode = service.GetChildren(node.Key);
-                 
-                    
-                        int parentitartionID = -1;
-                        foreach (var childNode in rootnode)
-                        {
-                            parentitartionID = -1;
-                            if (childNode.Name == yearContent.Name)
-                            {
-                                parentContentID = childNode.Id;
-                                parentitartionID = childNode.Id;
-                                break;
-                            }
 
-
-                        }
-                        if (parentitartionID == -1)
-                        {
-                            try
-                            {
-                                yearContent.SetValue("issueDate", monthissued.ToShortDateString());
-                                service.Save(yearContent);
-                            }
-                            catch { }
-                            
-                            parentContentID = yearContent.Id;
-                        }
-                    
-                    currentnodeYear = Issuedyear;
-                }
-                var volumecontent = service.CreateContent(newsletterobj.ChildNodeName, parentContentID, VOLUME_DOC_TYPE);               
-                var contentnode = service.GetChildren(parentContentID);
-                int innercontentID = -1;
-
-                
-                    int childitrationId = -1;
-                    foreach (var childNode in contentnode)
+                    IEnumerable<IContent> rootnodes = Service.GetChildren(node.Key);
+                   
+                    foreach (var childnode in rootnodes)
                     {
-                        childitrationId = -1;
-                        if (childNode.Name == volumecontent.Name)
+
+                        if (childnode.Name == issuedYear.ToString())
                         {
-                            innercontentID = childNode.Id;
-                            childitrationId = childNode.Id;
+                            parentContentID = childnode.Id;
                             break;
                         }
                     }
-                    if (childitrationId == -1)
+                    
+                    if ( parentContentID == -1)
                     {
+                        yearContent = Service.CreateContent(issuedYear.ToString(), node.Key, FOLDER_DOC_TYPE);
                         try
                         {
-                            volumecontent.SetValue("postedDate", newsletterobj.date.ToShortDateString());
-                            volumecontent.SetValue("content", newsletterobj.Content);
-                            service.Save(volumecontent);
+                            yearContent.SetValue("issueDate", monthIssued.ToShortDateString());
+                            Service.Save(yearContent);
                         }
                         catch { }
-                       
-                    }
 
+                        parentContentID = yearContent.Id;
+                    }
+                    currentnodeYear = issuedYear;
+                }
+                
+                var contentnodes = Service.GetChildren(parentContentID);
+                int innerContentID = -1;
+                foreach (var childNode in contentnodes)
+                {
+                    
+                    if (childNode.Name == newsletterobj.ChildNodeName)
+                    {
+                        innerContentID = childNode.Id;
+                        break;
+                    }
+                }
+                if (innerContentID == -1)
+                {
+                    var volumecontent = Service.CreateContent(newsletterobj.ChildNodeName, parentContentID, VOLUME_DOC_TYPE);
+                    if (node.Value == 372)
+                    {
+                        newsletterobj.Content = ParseContent(newsletterobj.Content);
+                    }
+                    try
+                    {
+                        volumecontent.SetValue("postedDate", newsletterobj.date.ToString());
+                        volumecontent.SetValue("content", newsletterobj.Content);
+                        Service.Save(volumecontent);
+                    }
+                    catch { }
+
+                }
                 
             }
             #endregion code to insert in to umbraco DB
-
         }
 
         internal List<NewsLetter> RetriveDatabaseValues(string sql)
         {
             using (SqlConnection ektronConn = new SqlConnection(ConfigurationManager.ConnectionStrings["Ektron.DbConnection"].ToString()))
             {
-               
-                SqlCommand cmd = new SqlCommand(sql, ektronConn);
-
-                ektronConn.Open();
+                List<NewsLetter> newsletterNodes = new List<NewsLetter>();
                 SqlDataReader reader = null;
+                SqlCommand cmd = new SqlCommand(sql, ektronConn);
+                ektronConn.Open();
                 reader = cmd.ExecuteReader();
-
-                List<NewsLetter> Newsletternodes = new List<NewsLetter>();
+                
                 while (reader != null && reader.Read())
                 {
                     NewsLetter newsletterobj = new NewsLetter();
                     newsletterobj.Content = reader["content_html"].ToString();
                     newsletterobj.date = DateTime.Parse(reader["date_created"].ToString());
                     newsletterobj.ChildNodeName = reader["content_title"].ToString();
-                    Match match = regex.Match(newsletterobj.ChildNodeName);
+                    Match match = Regex.Match(newsletterobj.ChildNodeName);
                     if (match.Success)
                     {
                         newsletterobj.ChildNodeName = match.Groups["info"].Value;
                     }
                     newsletterobj.ChildNodeName = newsletterobj.ChildNodeName.ToLower().Replace("vol", "Vol ");
-                    Newsletternodes.Add(newsletterobj);
+                    newsletterNodes.Add(newsletterobj);
                 }
-                return Newsletternodes;
+                return newsletterNodes;
 
             }
         }
@@ -185,15 +178,74 @@ namespace DBConversions
             var application = new ConsoleApplicationBase();
             application.Start(application, new EventArgs());
             var context = Umbraco.Core.ApplicationContext.Current;
-            //Write status for DatabaseContext
-            var databaseContext = context.DatabaseContext;
-            //Write status for Database object
-            var database = databaseContext.Database;
-            //Get the ServiceContext and the two services we are going to use
             var serviceContext = context.Services;
-            var contentTypeService = serviceContext.ContentTypeService;
+            //var contentTypeService = serviceContext.ContentTypeService;
             
             return (serviceContext.ContentService);
+        }
+
+        private string ParseContent(string rawcontent)
+        {
+            string newBannertable = string.Empty;
+            string oldBannertable = string.Empty;
+            string htmlcontent = rawcontent;
+            bool isbannerremoved = false;
+
+            //htmlcontent = htmlcontent.TrimStart();
+            if (htmlcontent.TrimStart().Substring(0, 6) == "<table")
+            {
+                int trindex = rawcontent.IndexOf("<tr>");
+                oldBannertable = rawcontent.Substring(trindex).TrimStart();
+                oldBannertable = oldBannertable.Substring(0, oldBannertable.IndexOf("</tr>") + 5);
+
+                rawcontent = rawcontent.Remove(trindex, oldBannertable.Length);
+                isbannerremoved = true;
+            }
+            else
+            {
+                 int index = rawcontent.IndexOf("<table border=\"0\" width=\"100%\">");
+                string endtableToRemove;
+                if (index != -1)
+                {
+
+                    newBannertable = rawcontent.Substring(index);
+                    if (newBannertable.Substring(0, 6).TrimStart() == "<table")
+                    {
+
+                        string tableToRemoveinit = newBannertable.Substring(0, newBannertable.IndexOf("</table>") + 8);
+
+                        newBannertable = newBannertable.Remove(0, tableToRemoveinit.Length);
+                        endtableToRemove = newBannertable.Substring(0, newBannertable.IndexOf("</table>") + 8);
+
+                        rawcontent = rawcontent.Remove(index, (endtableToRemove.Length + tableToRemoveinit.Length));
+                        isbannerremoved = true;
+                    }
+                }
+               
+            }
+            if (!isbannerremoved)
+            {
+               int index = rawcontent.IndexOf("<td valign=\"top\">");
+                string endtableToRemove;
+                if (index != -1)
+                {
+
+                    newBannertable = rawcontent.Substring(index + 17).TrimStart();
+                    if (newBannertable.Substring(0, 6).TrimStart() == "<table")
+                    {
+
+                        string tableToRemoveinit = newBannertable.Substring(0, newBannertable.IndexOf("</table>") + 8);
+
+                        newBannertable = newBannertable.Remove(0, tableToRemoveinit.Length);
+                        endtableToRemove = newBannertable.Substring(0, newBannertable.IndexOf("</table>") + 8);
+
+                        rawcontent = rawcontent.Remove(index + 17, (endtableToRemove.Length + tableToRemoveinit.Length));
+                        isbannerremoved = true;
+                    }
+                }
+            }
+
+            return rawcontent;
         }
     }
 }
