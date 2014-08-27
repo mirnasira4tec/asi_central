d692c8ab5e36dfab598c705831b73ad7e6533fc6
rrenjith <rrenjith@macrosoftindia.com>
2014-08-27 03:49:30 -0400

    Updating new get packages.
    added config files from webapplication and modified app.config

--- Projects/DB Conversions/src/DBConversions/Menu.cs
+++ Projects/DB Conversions/src/DBConversions/Menu.cs
@@ -21,10 +21,12 @@ namespace DBConversions
     {
 
         public static readonly int NEWSLETTER_PARENT_NODE_ID = 3226;
-        public static readonly string FOLDER_DOC_TYPE = "issue";
+        public static readonly string FOLDER_DOC_TYPE = "Issue";
         public static readonly string VOLUME_DOC_TYPE = "NewsItem";
         public static readonly int TEMPLATE_ID=1116;
         public static readonly  Regex Regex = new Regex(@"(?<info>.+)\s*(?<date>\d\d/\d+/\d+)");
+       
+        
         public IContentService Service = null;
        // public Dictionary<int, string> Childnodesdictionary = new Dictionary<int, string>();
         public Dictionary<int, int> nodeFolderIds = new Dictionary<int, int>()
@@ -36,8 +38,22 @@ namespace DBConversions
                 {3231,374},
                 {3232,370},
                 {3233,551},
-                {3234,552}
+                {3234,552},
+                {3373,542},
+                {3374,412}
             };
+        //public Dictionary<int, int> nodeFolderIds = new Dictionary<int, int>()
+        //    {
+        //        {3339,372},
+        //        {3340,548},
+        //        {3341,368},
+        //        {3342,546},
+        //        {3343,374},
+        //        {3344,370},
+        //        {3345,551},
+        //        {3346,552}
+                
+        //    };
         
         public Menu()
         {
@@ -74,7 +90,26 @@ namespace DBConversions
             #region code to insert in to umbraco DB
             foreach (NewsLetter newsletterobj in Newsletternodes)
             {
-                string issuedYear = newsletterobj.date.ToString("Y").Replace(',', ' ');
+                if (node.Value != 542)
+                {
+                    Match match = Regex.Match(newsletterobj.ChildNodeName);
+                    if (match.Success)
+                    {
+                        newsletterobj.ChildNodeName = match.Groups["info"].Value;
+                    }
+                    if(node.Value==551 ||node.Value==552)
+                    {
+                        string[] name = newsletterobj.ChildNodeName.Replace(". ",".").Split(' ');
+                        newsletterobj.ChildNodeName = name[0];
+
+                    }
+                    newsletterobj.ChildNodeName = newsletterobj.ChildNodeName.ToLower().Replace("vol", "Vol ");
+
+                }
+               
+                    newsletterobj.ChildNodeName = newsletterobj.ChildNodeName.Replace(".", "").Replace(",", "").Replace("  ", " ");
+               
+                string issuedYear = newsletterobj.date.ToString("Y").Replace(',', ' ').Replace("  "," ");
                 DateTime monthIssued = DateTime.Parse(string.Concat("01 ", issuedYear));
 
                 IContent yearContent = null;
@@ -159,12 +194,6 @@ namespace DBConversions
                     newsletterobj.Content = reader["content_html"].ToString();
                     newsletterobj.date = DateTime.Parse(reader["date_created"].ToString());
                     newsletterobj.ChildNodeName = reader["content_title"].ToString();
-                    Match match = Regex.Match(newsletterobj.ChildNodeName);
-                    if (match.Success)
-                    {
-                        newsletterobj.ChildNodeName = match.Groups["info"].Value;
-                    }
-                    newsletterobj.ChildNodeName = newsletterobj.ChildNodeName.ToLower().Replace("vol", "Vol ");
                     newsletterNodes.Add(newsletterobj);
                 }
                 return newsletterNodes;
