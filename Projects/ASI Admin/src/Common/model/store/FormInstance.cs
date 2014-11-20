bae3bc4d758b16ab664f8a84f3f163e44e3e2f01
kphani <kphani@macrosoftindia.com>
2014-11-20 02:50:11 -0500

    ASCN-1831: Update the model to add validation rules to match column size in the database

--- Projects/ASI Admin/src/Common/model/store/FormInstance.cs
+++ Projects/ASI Admin/src/Common/model/store/FormInstance.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using asi.asicentral.Resources;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -24,29 +25,39 @@ namespace asi.asicentral.model.store
 		[Display(Description = "User Email", Prompt = "The user email")]
 		[Required(ErrorMessage = "The user email is required")]
 		[DataType(DataType.EmailAddress)]
+        [StringLength(500, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
         public string Email { get; set; }
+
 		[Display(Description = "Salutation", Prompt = "How to address the user")]
 		[Required(ErrorMessage = "You need a way to address the user")]
+        [StringLength(200, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
 		public string Salutation { get; set; }
 
 		[Display(Description = "Greetings", Prompt = "A small introduction to the order")]
 		[Required(ErrorMessage = "You need a greeting message for the email")]
 		[DataType(DataType.MultilineText)]
+        [StringLength(10000, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
         public string Greetings { get; set; }
+
+        [StringLength(150, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
         public string ExternalReference { get; set; }
 
 		[Display(Description = "Order Total", Prompt = "amount")]
 		[Required(ErrorMessage = "You need to specify the order amount")]
 		[DataType(DataType.Currency)]
 		public decimal Total { get; set; }
+
 		public virtual StoreOrderDetail OrderDetail { get; set; }
 		public int? OrderDetailId { get; set; }
 
 		[Display(Description = "Comments", Prompt = "Internal comments about the order")]
 		[DataType(DataType.MultilineText)]
+        [StringLength(10000, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
 		public string Comments { get; set; }
 
+        [StringLength(200, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
 		public string Sender { get; set; }
+
 		public virtual IList<FormValue> Values { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime UpdateDate { get; set; }
