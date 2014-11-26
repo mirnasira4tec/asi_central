30c3a16e7b12f8c1840f415280eb8da570d0a2a6
yperrin <yperrin@asicentral.com>
2014-11-25 22:25:50 -0500

    Changing model for form type to support custom phone and chat settings

--- Projects/ASI Admin/src/Common/model/store/FormType.cs
+++ Projects/ASI Admin/src/Common/model/store/FormType.cs
@@ -14,8 +14,7 @@ namespace asi.asicentral.model.store
         public string Implementation { get; set; }
         public string TermsAndConditions { get; set; }
         public string NotificationEmails { get; set; }
-        public string ChatSettings { get; set; }
-		public string Phone { get; set; }
+        public int? ContextId { get; set; }
         public bool IsASINumberFlag { get; set; }
 	    public virtual IList<FormInstance> FormInstances { get; set; }
         public DateTime CreateDate { get; set; }
