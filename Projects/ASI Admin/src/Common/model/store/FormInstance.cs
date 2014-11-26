b9d2bbaf6e914c3033e023711c7170220dfa4cfe
yperrin <yperrin@asicentral.com>
2014-11-26 17:29:34 -0500

    Changed labels

--- Projects/ASI Admin/src/Common/model/store/FormInstance.cs
+++ Projects/ASI Admin/src/Common/model/store/FormInstance.cs
@@ -22,23 +22,22 @@ namespace asi.asicentral.model.store
         public virtual FormType FormType { get; set; }
         public int FormTypeId { get; set; }
 
-		[Display(Description = "User Email", Prompt = "The user email")]
+		[Display(Name = "User Email", Prompt = "The user email")]
 		[Required(ErrorMessage = "The user email is required")]
 		[DataType(DataType.EmailAddress)]
         [StringLength(500, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
         public string Email { get; set; }
 
-        [Display(Description = "Notification", Prompt = "The people to notify when order is through")]
-        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Notification Emails", Prompt = "The people to notify when order is through")]
         [StringLength(500, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
         public string NotificationEmails { get; set; }
 
-		[Display(Description = "Salutation", Prompt = "How to address the user")]
+		[Display(Name = "Salutation", Prompt = "How to address the user")]
 		[Required(ErrorMessage = "You need a way to address the user")]
         [StringLength(200, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
 		public string Salutation { get; set; }
 
-		[Display(Description = "Greetings", Prompt = "A small introduction to the order")]
+		[Display(Name = "Greetings", Prompt = "A small introduction to the order")]
 		[Required(ErrorMessage = "You need a greeting message for the email")]
 		[DataType(DataType.MultilineText)]
         [StringLength(10000, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
@@ -47,7 +46,7 @@ namespace asi.asicentral.model.store
         [StringLength(150, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
         public string ExternalReference { get; set; }
 
-		[Display(Description = "Order Total", Prompt = "amount")]
+		[Display(Name = "Order Total", Prompt = "amount")]
 		[Required(ErrorMessage = "You need to specify the order amount")]
 		[DataType(DataType.Currency)]
 		public decimal Total { get; set; }
@@ -55,7 +54,7 @@ namespace asi.asicentral.model.store
 		public virtual StoreOrderDetail OrderDetail { get; set; }
 		public int? OrderDetailId { get; set; }
 
-		[Display(Description = "Comments", Prompt = "Internal comments about the order")]
+		[Display(Name = "Comments", Prompt = "Internal comments about the order")]
 		[DataType(DataType.MultilineText)]
         [StringLength(10000, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FieldLength")]
 		public string Comments { get; set; }
