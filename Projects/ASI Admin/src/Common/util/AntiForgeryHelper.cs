c4cc5512a1702dcc04fcaf43dcd2651402a6c542
kphani <kphani@macrosoftindia.com>
2015-03-25 05:01:18 -0400

    ASCN-2051: We are getting a consistent error message on the live site

---
+++ Projects/ASI Admin/src/Common/util/AntiForgeryHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace asi.asicentral.util
+{
+    public static class ASIHtml
+    {
+        public static MvcHtmlString AntiForgeryToken(this System.Web.Mvc.HtmlHelper helper)
+        {
+            return new MvcHtmlString("<input name='__RequestVerificationToken' type='hidden' value='' />");
+        }
+    }
+}
