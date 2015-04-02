079798a6701a3a4086556f2ec9467f0e73688be2
kphani <kphani@macrosoftindia.com>
2015-04-02 09:47:00 -0400

    ASCN-2200: Fix issues in check-out flow if any

---
+++ Projects/ASI Admin/src/Common/util/store/digitalmarketing/DigitalMarketingHelper.cs
@@ -0,0 +1,28 @@
+﻿using asi.asicentral.model.store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using asi.asicentral.interfaces;
+using System.Web.Mvc;
+using asi.asicentral.Resources;
+
+namespace asi.asicentral.util.store
+{
+    public class DigitalMarketingHelper
+    {
+        //Supplement options
+        public static readonly int AD_WORDS_INCREMENT = 50;
+        public static readonly int DIGITAL_MARKETING_CONTEXTID = 24;
+
+        public static IList<SelectListItem> GetGoogleAdWordOptions()
+        {
+            IList<SelectListItem> selectedItems = new List<SelectListItem>();
+            for (int option = 2; option <= 15; option++)
+            {
+                selectedItems.Add(new SelectListItem() { Text = option.ToString(), Value = option.ToString(), Selected = false });
+            }
+            return selectedItems;
+        }
+    }
+}
