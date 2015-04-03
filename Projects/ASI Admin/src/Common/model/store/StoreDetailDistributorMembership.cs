16362414f6cca872e04e55cd4d2dc32b597afa19
kphani <kphani@macrosoftindia.com>
2015-04-03 01:49:02 -0400

    Added identifiers for the Digital Marketing Success

--- Projects/ASI Admin/src/Common/model/store/StoreDetailDistributorMembership.cs
+++ Projects/ASI Admin/src/Common/model/store/StoreDetailDistributorMembership.cs
@@ -10,8 +10,10 @@ namespace asi.asicentral.model.store
 {
     public class StoreDetailDistributorMembership : StoreDetailApplication
     {
-        //In the below list of product id's 5 to 8 are distributor products and 29 to 31 are Proforma products
-        public static int[] Identifiers = new int[] { 5, 6, 7, 8, 29, 30 , 31, 81 };
+        //In the below list of product id's 5 to 8 are distributor products 
+        //and 29 to 31 are Proforma products
+        //96,97
+        public static int[] Identifiers = new int[] { 5, 6, 7, 8, 29, 30 , 31, 81, 96, 97, 98 };
         
         public StoreDetailDistributorMembership()
         {
