a72e9ac21ef561c1b4966a2ae70706075b2ac2a0
Yann Perrin <perrin.yann@googlemail.com>
2015-04-22 18:10:12 -0400

    ASCN-2127 Adding list of products

---
+++ Projects/ASI Admin/src/Common/util/store/SalesForm/SalesFormHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace asi.asicentral.util.store
+{
+	public static class SalesFormHelper
+	{
+		public static readonly string[,] PRODUCTS = { { "ESP Website", "ESPW-60" }, { "Additional ESP License", "ESPL-60" }, { "Company Store", "CPYS-49.99" }, 
+			{ "Distributor Membership", "DIST-29.99" }, {"Distributor Package – Basic","DIST-149.99"}, {"Distributor Package- Standard","DIST-199.99"}, {"Distributor Package – Executive","DIST-229.99"}, {"Distributor Package- Premium","299.99"},
+			{ "ESP Social – Starter", "ESPS-49.99"}, {"ESP Social – Advanced","ESPS-99.99"}, {"ESP Social – Pro", "ESPS-199.99"},
+			{"ASI Brand Builder - Standard", "ABRB-399.99"}, {"ASI Brand Builder - Plus", "ABRB-599.99"}, {"ASI Brand Builder - Platinum", "ABRB-999.99"},
+			{"Trafficbuilder Plus – Membership", "TFBP-249.99"}, {"Trafficbuilder Plus – Basic", "TFBP-499.99"}, {"Trafficbuilder Plus – Standard", "TFBP-999.99"}, {"Trafficbuilder Plus- Executive", "TFBP-249.99"},
+			{"Decorator Membership - Standard","DECM-49.99"}, {"Decorator Membership - Plus", "DECM-99.99"}, {"Decorator Membership - Pro", "DECM-149.99"}};
+
+		public static readonly string[,] FEES = { { "Application Fee", "APPF-150" }, { "Set Up Fee", "STPF-199" } };
+	}
+}
