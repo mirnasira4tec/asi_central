bca0fd978c0ae2d8ca714e0988aef2f2bb30d4e7
Yann Perrin <perrin.yann@googlemail.com>
2015-07-15 16:30:10 -0400

    Removing unused call to Personify to get CC profile

--- Projects/ASI Admin/src/Common/services/PersonifyService.cs
+++ Projects/ASI Admin/src/Common/services/PersonifyService.cs
@@ -188,7 +188,7 @@ namespace asi.asicentral.services
 			order.ExternalReference = companyInfo.MasterCustomerId;
             //Add credit card to the company
 			//@todo CC Personify temporary measure, always add the credit card. No longer checking if (profile == string.Empty) from string profile = PersonifyClient.GetCreditCardProfileId(order.GetASICompany(), companyInfo, creditCard);
-			profile = PersonifyClient.SaveCreditCard(order.GetASICompany(), companyInfo, creditCard);
+			var profile = PersonifyClient.SaveCreditCard(order.GetASICompany(), companyInfo, creditCard);
             log.Debug(string.IsNullOrWhiteSpace(profile) ?
                 "Fail to save the credit." : string.Format("Saved credit profile id : {0}", profile));
             if (string.IsNullOrEmpty(profile)) throw new Exception("Credit card can't be saved to Personify.");
