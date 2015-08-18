d8083c770b2a510ebf757bbac949dec996dc9a83
kphani <kphani@macrosoftindia.com>
2015-08-18 07:53:07 -0400

    SHOW-1162: Create Admin UI for Show - Attendee Data

---
+++ Projects/ASI Admin/src/Web Application/Models/Show/ShowCompaniesModel.cs
@@ -0,0 +1,22 @@
+﻿using asi.asicentral.model.show;
+using asi.asicentral.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asi.asicentral.web.models.show
+{
+    public class ShowCompaniesModel 
+    {
+        public ShowCompaniesModel()
+        {
+            ShowAttendees = new List<ShowAttendee>();
+        }
+
+        public ShowASI Show { get; set; }
+        public IList<ShowAttendee> ShowAttendees { get; set; }
+    }
+}
