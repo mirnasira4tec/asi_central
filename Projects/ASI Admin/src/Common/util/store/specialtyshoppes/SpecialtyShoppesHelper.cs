a2c555169ca2a9e8fa66b90663e3791d9fc7c112
kphani <kphani@macrosoftindia.com>
2015-09-25 04:51:44 -0400

    ASCN-2558: Code added for specialty shoppes

---
+++ Projects/ASI Admin/src/Common/util/store/specialtyshoppes/SpecialtyShoppesHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using asi.asicentral.Resources;
+
+namespace asi.asicentral.util.store
+{
+    public class SpecialtyShoppesHelper
+    {
+        public static readonly decimal[] SpecialtyShoppes_Cost = { 49, 99 };
+
+        public static IList<SelectListItem> GetQuantityOptions()
+        {
+            IList<SelectListItem> quantityOptions = new List<SelectListItem>();
+            quantityOptions.Add(new SelectListItem() { Text = "Healthcare Store", Value = "1" });
+            quantityOptions.Add(new SelectListItem() { Text = "Education Store", Value = "2" });
+            quantityOptions.Add(new SelectListItem() { Text = "Finance Store", Value = "3" });
+            quantityOptions.Add(new SelectListItem() { Text = "Construction Store", Value = "4" });
+            quantityOptions.Add(new SelectListItem() { Text = "Technology Store", Value = "5" });
+            quantityOptions.Add(new SelectListItem() { Text = "5 Pack of Stores", Value = "6" });
+            return quantityOptions;
+        }
+
+        public static decimal GetCost(int optionId)
+        {
+            return SpecialtyShoppes_Cost[optionId];
+        }
+    }
+}
