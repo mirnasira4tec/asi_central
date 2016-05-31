da3a8a26431453cf3b5aa5d14068ebcec15214b2
rohan1601 <rohan.kathe@a4technology.com>
2016-05-31 09:54:47 -0400

    ASCN-2835 : Implementation of restricted access to admin functionality.

---
+++ Projects/ASI Admin/src/Web Application/Helpers/AuthorizedUser.cs
@@ -0,0 +1,33 @@
+﻿using asi.asicentral.model.store;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace asi.asicentral.web
+{
+    public class Authorized
+    {
+        private readonly string[] allowedusers = ConfigurationManager.AppSettings["AuthorizedUsers"].Split(';');
+
+        public bool IsAuthorizedUser()
+        {
+            WindowsIdentity identity = HttpContext.Current.Request.LogonUserIdentity;
+            var Name = identity.Name;
+            var AuthorizedUser = false;
+            if (allowedusers.Any())
+            {
+                for (int i = 0; i < allowedusers.Count(); i++)
+                {
+                    if (Name.Equals(allowedusers[i]))
+                        AuthorizedUser = true; 
+                    else
+                        AuthorizedUser = false;
+                }
+            }
+            return AuthorizedUser;
+        }
+    }
+}
