9a998e6119f8f7a75a494e010434cd91a62dc455
ReenaPrajapati <reena.prajapati@A4technology.com>
2016-07-07 02:37:34 -0400

    ASCN-2858 :Added few more line items for show page

--- Projects/ASI Admin/src/Common/util/store/SalesForm/SalesFormHelper.cs
+++ Projects/ASI Admin/src/Common/util/store/SalesForm/SalesFormHelper.cs
@@ -1,8 +1,8 @@
 ﻿namespace asi.asicentral.util.store
 {
-	public static class SalesFormHelper
-	{
-		public static readonly string[,] PRODUCTS = { 
+    public static class SalesFormHelper
+    {
+        public static readonly string[,] PRODUCTS = { 
             { "ESP Website", "ESPW-60", "ESP (ALL) Product" }, 
             { "Additional ESP License", "ESPL-60", "" }, 
             { "Company Store", "CPYS-49.99", "ESP (ALL) Product" },
@@ -19,38 +19,44 @@
             { "Social Boost – Starter", "SOBO-49",""},
             { "Social Boost – Advanced", "SOBO-99",""},
             { "Social Boost – Pro", "SOBO-199",""},
+            {"Website Maintenance 1 hr","WEBM-120",""},
+            {"Website Maintenance 2 hr","WEBM-240",""},
+            {"Website Maintenance 3 hr","WEBM-360",""},
+            {"Website Maintenance 4 hr","WEBM-480",""},
+            {"Website Maintenance 6 hr","WEBM-720",""},
+            {"Website Maintenance 12 hr","WEBM-1440",""},
 			{ "Decorator Membership - Standard", "DECM-49.99", "Decorator Membership" }, 
             { "Decorator Membership - Plus", "DECM-99.99", "Decorator Membership" }, 
             { "Decorator Membership - Pro", "DECM-149.99", "Decorator Membership" } };
 
-		public static readonly string[,] FEES = { { "Application Fee", "APPF-150", "" }, { "Set Up Fee", "STPF-199", "" }, 
+        public static readonly string[,] FEES = { { "Application Fee", "APPF-150", "" }, { "Set Up Fee", "STPF-199", "" }, 
              { "Custom Package Pro", "CPRO-299", "Custom Websites Pro" }, { "Custom Package Master", "CPMT-599", "Custom Websites Master" },  { "Custom Package Genius", "CPGE-999", "Custom Websites Genius" } };
 
         public static readonly int SALES_FORM_PRODUCT_ID = 99;
 
-		public static string GetProductDescription(string code)
-		{
-			var description = string.Empty;
-			for (var i = 0; i < PRODUCTS.GetLength(0); i++)
-			{
-				if (PRODUCTS[i,1].Equals(code))
-				{
-					description = PRODUCTS[i, 0];
-					break;
-				}
-			}
-			if (description.Equals(string.Empty))
-			{
-				for (var i = 0; i < FEES.GetLength(0); i++)
-				{
-					if (FEES[i, 1].Equals(code))
-					{
-						description = FEES[i, 0];
-						break;
-					}
-				}				
-			}
-			return description;
-		}
-	}
+        public static string GetProductDescription(string code)
+        {
+            var description = string.Empty;
+            for (var i = 0; i < PRODUCTS.GetLength(0); i++)
+            {
+                if (PRODUCTS[i, 1].Equals(code))
+                {
+                    description = PRODUCTS[i, 0];
+                    break;
+                }
+            }
+            if (description.Equals(string.Empty))
+            {
+                for (var i = 0; i < FEES.GetLength(0); i++)
+                {
+                    if (FEES[i, 1].Equals(code))
+                    {
+                        description = FEES[i, 0];
+                        break;
+                    }
+                }
+            }
+            return description;
+        }
+    }
 }
