5e7b06406df46e819c83525d523b0b42e5f570c6
mzhang-asi <mzhang@asicentral.com>
2017-10-20 11:33:09 -0400

    Update for EMS order in store.

--- Projects/ASI Admin/src/Common/interfaces/IBackendService.cs
+++ Projects/ASI Admin/src/Common/interfaces/IBackendService.cs
@@ -27,13 +27,15 @@ namespace asi.asicentral.interfaces
 
 		string SaveCreditCard(StoreOrder order, CreditCard creditCard);
 
-		/// <summary>
-		/// Used to get a list of credit cards for an existing company
-		/// </summary>
-		/// <param name="company">The store company record to get the list for</param>
-		/// <param name="asiCompany">The company code the card is for (ASI, ASI Show, ASI Canada</param>
-		/// <returns></returns>
-	    IEnumerable<StoreCreditCard> GetCompanyCreditCards(StoreCompany company, string asiCompany);
+        void SaveCreditCardInfo(StoreOrder order);
+
+        /// <summary>
+        /// Used to get a list of credit cards for an existing company
+        /// </summary>
+        /// <param name="company">The store company record to get the list for</param>
+        /// <param name="asiCompany">The company code the card is for (ASI, ASI Show, ASI Canada</param>
+        /// <returns></returns>
+        IEnumerable<StoreCreditCard> GetCompanyCreditCards(StoreCompany company, string asiCompany);
 
         CompanyInformation GetCompanyInfoByAsiNumber(string asiNumber);
 
