f5887a16388f1f42194060e72ca016a3da3716ab
iarunkverma <iarunkverma@gmail.com>
2018-05-24 10:46:48 -0400

    Verfiy personify Service

---
+++ Projects/ASI Admin/src/Common/util/Utility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+
+namespace asi.asicentral.util
+{
+  public static  class Utility
+    {
+        public static bool IsServiceAvailable(string url)
+        {
+            bool isServiceRunning = false;
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                var response = (HttpWebResponse)request.GetResponse();
+                if (response != null && response.StatusCode == HttpStatusCode.OK)
+                {
+                    isServiceRunning = true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return isServiceRunning;
+        }
+
+        public static bool IsPersonifyAvailable(bool usePersonify, string url)
+        {
+            if (!usePersonify)
+            {
+                usePersonify = false;
+            }
+            else
+            {
+                var isPersonifyRunning =IsServiceAvailable(url);
+                if (!isPersonifyRunning)
+                {
+                    usePersonify = false;
+                }
+            }
+            return usePersonify;
+        }
+    }
+}
