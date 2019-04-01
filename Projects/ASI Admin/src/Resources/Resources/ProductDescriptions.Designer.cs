497a10c0091318a95685c72e496425806f137c77
iarunkverma <iarunkverma@gmail.com>
2019-04-01 07:36:40 -0400

    ASCN-3394 Updating Store - ESP Websites

--- Projects/ASI Admin/src/Resources/Resources/ProductDescriptions.Designer.cs
+++ Projects/ASI Admin/src/Resources/Resources/ProductDescriptions.Designer.cs
@@ -19,7 +19,7 @@ namespace asi.asicentral.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ProductDescriptions {
@@ -187,7 +187,7 @@ namespace asi.asicentral.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to ESP Websites™ are functional and user-friendly websites pre-loaded with products from ESP®. Motivate your customers to stay, enjoy and shop with all of these great website features:.
+        ///   Looks up a localized string similar to ESP Websites™ are functional and user-friendly websites pre-loaded with products from ESP&lt;sup&gt;&amp;reg;&lt;/sup&gt;. Motivate your customers to stay, enjoy and shop with all of these great website features:.
         /// </summary>
         public static string ProductESPWebsites {
             get {
